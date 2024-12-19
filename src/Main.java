53882741bfe0e549d8d284932925a46863c1fb7e
LuigiJobDev <luigi.guernelli.dev@gmail.com>
2024-12-19 19:00:17 +0100

    First Java Commit for Profile Card.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        //Instanza la classe Scheda
+        Scheda scheda = new Scheda();
+        scheda.printScheda();
+    }
+}
---
+++ src/Scheda.java
@@ -0,0 +1,17 @@
+public class Scheda {
+        String Name = "Luigi Dev";
+        int insertData = 12345678;
+        int ID = 23;
+        int Age = 34;
+        float FinalVote = 72f;
+        char Grade = 'B';
+
+        public void printScheda(){
+                System.out.println("Scheda: ");
+                System.out.println("Name: " + Name);
+                System.out.println("Insert Data: " + insertData);
+                System.out.println("Age: " + Age);
+                System.out.println("Final Vote: " + FinalVote);
+                System.out.println("Grade: " + Grade);
+        }
+}
